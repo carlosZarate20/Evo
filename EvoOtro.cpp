b64a5fe1efe1f6b0855ded00e8c9d6ba8766b529
Carlos Zarate <u201314894@upc.edu.pe>
2016-04-26 17:28:25 -0500

    Primer commit

---
+++ EvoOtro.cpp
@@ -0,0 +1,15 @@
+// EvoOtro.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int x = 2, y = 4;
+
+int main()
+{
+	cout << "Hola mundo" << endl;
+	cout << x*y;2
+    return 0;
+}
